73a6646a1286d706f10dabb6f4daddf5b629f624
Atharva-techstuff <125642168+Atharva-techstuff@users.noreply.github.com>
2023-04-07 08:31:26 +0000

    Remove alternate prop everywhere

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -157,7 +157,7 @@ export const Home = () => {
       />
       <ProjectSummary
         id="project-2"
-        alternate
+        // alternate
         sectionRef={projectTwo}
         visible={visibleSections.includes(projectTwo.current)}
         index={2}
@@ -195,7 +195,7 @@ export const Home = () => {
       />
       <ProjectSummary
         id="project-4"
-         alternate
+        //  alternate
         sectionRef={projectFour}
         visible={visibleSections.includes(projectFour.current)}
         index={4}
@@ -233,7 +233,7 @@ export const Home = () => {
       />
       <ProjectSummary
         id="project-6"
-        alternate
+        // alternate
         sectionRef={projectSix}
         visible={visibleSections.includes(projectSix.current)}
         index={6}
